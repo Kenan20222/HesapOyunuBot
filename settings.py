c5146ce8a5621710efd474e0edbe42fa6c79b7f8
Kenan20222 <104547432+Kenan20222@users.noreply.github.com>
2022-06-25 12:41:19 +0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-TOKEN = "TOKEN YAZ"
+TOKEN = "5547681407:AAGnpaU8RvDl7MwUKM_boKsekiMf8SqGm50"
 with open('hüsnü.reqemlist.txt', 'r', encoding='utf-8') as file:
     word_list = file.read().splitlines()
